c7d44a58590e0592cd0f3f798a0a574fd9e805bd
Bbvmex <73261722+Bbvmex@users.noreply.github.com>
2021-11-29 16:38:47 -0300

    Update api_roma.py

--- api_roma.py
+++ api_roma.py
@@ -14,7 +14,7 @@ users = {
 parser = reqparse.RequestParser()
 parser.add_argument('username', type=str, help='User invalid', required = True)
 parser.add_argument('password', type=str, help='Invalid Password')
-
+parser.add_argument('tester', type=int, help='Liar')
 
 
 def abort_if_user_doesnt_exist(username):
@@ -22,12 +22,11 @@ def abort_if_user_doesnt_exist(username):
         abort(404, message='User doesnt exist')
 
 
-
-
 class User(Resource):
     def get(self):
         args = parser.parse_args()
         username = args['username']
+        print(args['tester'])
         abort_if_user_doesnt_exist(username)
         return users[username], 200
 
@@ -37,6 +36,13 @@ class User(Resource):
         password = args['password']
         users[username] = {'password': password}
         return users, 200
+
+    def delete(self):
+        args = parser.parse_args()
+        username = args['username']
+        del(users[username])
+        return users, 200
+
 
         
 api.add_resource(User, '/user/')
