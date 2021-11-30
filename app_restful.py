c92d4247e0924c8606c4ce7f7c8c197db87532e2
Bbvmex <73261722+Bbvmex@users.noreply.github.com>
2021-11-30 16:10:45 -0300

    Organization
    Removed redundant calls from main files

--- app_restful.py
+++ app_restful.py
@@ -1,30 +1,11 @@
-from itertools import product
-from typing import Dict
-from flask import Flask, jsonify, request
-from flask_restful import Api, Resource, reqparse, abort
-from flask_restful import fields, marshal_with
-from pymongo import MongoClient
-import bcrypt
-from common.common import UserExists, EmptyCart, ProductInCart, genReturnJson, ProductExists, verifyPw, verifyCredentials
-import json, bson
+from flask import Flask
+from flask_restful import Api
 from resources.user import User
 from resources.cart import Cart
 from resources.product import Product
-from db.db import users, carts, products
 
 app = Flask(__name__)
 api = Api(app)
-
-'''
-class MyEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, bson.ObjectId):
-            return str(obj)
-        if isinstance(obj, bytes):
-            return str(obj, encoding='utf-8')
-        return super(MyEncoder, self).default(obj) 
-
-app.json_encoder = MyEncoder'''
 
 api.add_resource(User, '/user/')
 api.add_resource(Cart, '/cart/')
--- db/db.py
+++ db/db.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 import certifi
 
 connection_string = "mongodb+srv://teste:teste_dev@hugovm-dev.vta9r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-client = MongoClient(connection_string, tlsCAFile=certifi.where())
+client = MongoClient(connection_string, tlsCAFile=certifi.where(), serverSelectionTimeoutMS=5000)
 db = client.starwars_shop
 users = db["users"]
 products = db["products"]
