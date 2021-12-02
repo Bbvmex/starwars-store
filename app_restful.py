8bd5d5c4985091af909755c4c50c0eacfd6e164e
Bbvmex <73261722+Bbvmex@users.noreply.github.com>
2021-12-01 22:45:16 -0300

    Added new authentication method
    Added new library for authentication and generate user access token

--- app_restful.py
+++ app_restful.py
@@ -1,15 +1,23 @@
 from flask import Flask
 from flask_restful import Api
-from resources.user import User
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+import json
+
+from resources.user import User, LogIn
 from resources.cart import Cart
 from resources.product import Product
 
 app = Flask(__name__)
+app.config.from_file('.env', load=json.load)
+
 api = Api(app)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
 
 api.add_resource(User, '/user/')
+api.add_resource(LogIn, '/user/login/')
 api.add_resource(Cart, '/cart/')
 api.add_resource(Product, '/product/')
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug = True)
--- resources/user.py
+++ resources/user.py
@@ -1,6 +1,9 @@
 from flask_restful import Resource, reqparse, abort
 from flask_restful import fields, marshal_with
-import bcrypt
+from flask_bcrypt import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token, jwt_required
+import datetime
+
 from db.db import users, carts
 from common.common import UserExists
 
@@ -9,16 +12,16 @@ user_args.add_argument('username', type=str, required=True, help='Username requi
 user_args.add_argument('password', type=str, required=True)
 
 class User(Resource):
+    @jwt_required
     def get(self):
         args = user_args.parse_args()
         username = args['username']
-        password = args['password']
         if not UserExists(username):
             abort(404, message='User does not exist')
         return users.find_one({'username': username},
                             {'_id': False,
                             'password': False}), 200
-
+    @jwt_required
     def delete(self):
         args = user_args.parse_args()
         username = args['username']
@@ -36,7 +39,7 @@ class User(Resource):
         if UserExists(username):
             abort(404, message='User already exists')
         
-        hashed_pw = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        hashed_pw = generate_password_hash(password)
 
         users.insert_one({
             'username': username,
@@ -47,7 +50,26 @@ class User(Resource):
             'username': username,
             'products': {}
         })
-
 #        if app.debug:
 #            return users
         return 'User added', 200
+
+# Generates the access token for the user.
+# It will be used as a request argument to access the database
+class LogIn(Resource):
+    def post(self):
+        args = args = user_args.parse_args()
+        username = args['username']
+        password = args['password']
+
+        if not UserExists(username):
+            abort(404, message='User does not exist')
+        
+        stored_pw = users.find_one({'username': username})['password']
+        authorized = check_password_hash(stored_pw, password)
+        if not authorized:
+            return 'User or password invalid', 401
+        
+        expires = datetime.timedelta(hours=1)
+        access_token = create_access_token(identity=username, expires_delta=expires)
+        return {'token': access_token}, 200
