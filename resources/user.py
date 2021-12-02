3413da618d99355a1c86e64b18c6047311b86e90
Bbvmex <73261722+Bbvmex@users.noreply.github.com>
2021-12-02 07:19:42 -0300

    New auth changes
    Still notn woring

--- resources/user.py
+++ resources/user.py
@@ -1,27 +1,42 @@
 from flask_restful import Resource, reqparse, abort
 from flask_restful import fields, marshal_with
 from flask_bcrypt import generate_password_hash, check_password_hash
-from flask_jwt_extended import create_access_token, jwt_required
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from flask_httpauth import HTTPBasicAuth
 import datetime
 
 from db.db import users, carts
 from common.common import UserExists
 
+auth = HTTPBasicAuth()
+
 user_args = reqparse.RequestParser()
 user_args.add_argument('username', type=str, required=True, help='Username required')
 user_args.add_argument('password', type=str, required=True)
 
+@auth.verify_password
+def verify_password(username, password):
+    user = users.find_one({'username': username})
+    stored_pw = user['password']
+    authorized = check_password_hash(stored_pw, password)
+    if not user or not authorized:
+        return False
+    #g.user = user
+    return True
+
 class User(Resource):
-    @jwt_required
     def get(self):
+        decorators = [jwt_required]
         args = user_args.parse_args()
         username = args['username']
+        password = args['password']
         if not UserExists(username):
             abort(404, message='User does not exist')
         return users.find_one({'username': username},
                             {'_id': False,
                             'password': False}), 200
-    @jwt_required
+
+    #@jwt_required
     def delete(self):
         args = user_args.parse_args()
         username = args['username']
@@ -38,9 +53,7 @@ class User(Resource):
         password = args['password']
         if UserExists(username):
             abort(404, message='User already exists')
-        
         hashed_pw = generate_password_hash(password)
-
         users.insert_one({
             'username': username,
             'password': hashed_pw,
